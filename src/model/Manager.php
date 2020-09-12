7ce71d0cb4282dc85d9c6028c93cef1c9f3cb15d
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-12 22:42:21 +0200

    créer un Class Manager pour BDD
    créer un Class Manager pour connecter à la bdd.

--- src/model/Manager.php
+++ src/model/Manager.php
@@ -1,1 +1,17 @@
 <?php
+//connection à la base de donnée
+class Manager
+{
+	protected function dbConnection() 
+	{
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=jeansblog;charset=utf8', 'root', '');
+			return $bdd;
+		}
+		catch (Exception $e)
+		{
+			die('Erreur : ' .$e->getMessage());
+		}
+	}
+}
