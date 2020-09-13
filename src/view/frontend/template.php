c1d522a7d5754ce24d2a6b78b9bf7f30d3c1fee6
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-13 02:17:39 +0200

    créer le menu de navigation avec bootstrap
    créer le menu de navigation avec bootstrap, ajouter des links de navigation, logo sous le format de texte.

--- src/view/frontend/template.php
+++ src/view/frontend/template.php
@@ -13,12 +13,28 @@
 </head>
 <body>
   <header>
-   <div class="container">
-   <a class="navbar-brand" href="index.php">
-    JEAN-FORTEROCHE
-  </a>
-   </div>
-
+   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="index.php">JEAN-FORTEROCHE</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Déconnexion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Connexion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inscription</a>
+          </li>
+      </ul>
+    </div>
+  </nav>
 </header>
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
