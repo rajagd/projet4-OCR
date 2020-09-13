fb4301692dd5f55acabde138892232ba0e7a7fa7
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-13 14:24:37 +0200

    créer une page header.php
    créer une page header.php pour les contenus sur le page d'accueil..

---
+++ src/view/frontend/header.php
@@ -0,0 +1,25 @@
+<?php $title = 'JEAN FORTEROCHE'; ?>
+<?php ob_start();?>   
+<div id="p-article" class="container">
+<h2>Bienvenue </h2>
+        <p class="p-article">Bonjour, Ce blog est édité sous la responsabilité de Jean Forteroche. Je vous remercie de votre visite et des commentaires que vous rédigerez sous mes billets. Merci de respecter les règles fixées par notre soin: soyez courtois, concis, pertinent et signez vos commentaires! Au plaisir de vous lire. Vous pouvez aussi me contacter en cliquant ici</p><br><br>
+        <a href=""><button class="btn btn-secondary">Contact</button></a>
+</div>
+<div id="p-article" class="container">
+<h3>Chapitre 1 </h3>
+        <p class="p-article">Duis cursus aliquam purus, id bibendum velit vulputate sit amet. Duis luctus scelerisque arcu, maximus viverra risus eleifend sit amet. Donec id dui egestas, auctor sem at, mollis purus. In purus purus, euismod sit amet velit vitae, euismod dapibus nisi. Morbi mollis molestie eros. Vivamus a odio nulla. Praesent turpis nisi, gravida eget neque cursus, sagittis molestie ligula. Integer ac rutrum ipsum, ac cursus augue. Donec ut sollicitudin nisl, et finibus est. Cras sodales dui diam, eu tincidunt dolor posuere non.</p><br><br>
+        <a href=""><button class="btn btn-secondary">Lire tout!</button></a>
+</div>
+<div id="p-article" class="container">
+<h3>Chapitre 2 </h3>
+        <p class="p-article">Duis cursus aliquam purus, id bibendum velit vulputate sit amet. Duis luctus scelerisque arcu, maximus viverra risus eleifend sit amet. Donec id dui egestas, auctor sem at, mollis purus. In purus purus, euismod sit amet velit vitae, euismod dapibus nisi. Morbi mollis molestie eros. Vivamus a odio nulla. Praesent turpis nisi, gravida eget neque cursus, sagittis molestie ligula. Integer ac rutrum ipsum, ac cursus augue. Donec ut sollicitudin nisl, et finibus est. Cras sodales dui diam, eu tincidunt dolor posuere non.</p><br><br>
+        <a href=""><button class="btn btn-secondary">Lire tout!</button></a>
+</div>
+<div id="p-article" class="container">
+<h3>Chapitre 3 </h3>
+        <p class="p-article"> Duis cursus aliquam purus, id bibendum velit vulputate sit amet. Duis luctus scelerisque arcu, maximus viverra risus eleifend sit amet. Donec id dui egestas, auctor sem at, mollis purus. In purus purus, euismod sit amet velit vitae, euismod dapibus nisi. Morbi mollis molestie eros. Vivamus a odio nulla. Praesent turpis nisi, gravida eget neque cursus, sagittis molestie ligula. Integer ac rutrum ipsum, ac cursus augue. Donec ut sollicitudin nisl, et finibus est. Cras sodales dui diam, eu tincidunt dolor posuere non.</p><br><br>
+        <a href=""><button class="btn btn-secondary">Lire tout!</button></a>
+</div>
+
+  <?php $content = ob_get_clean(); ?>
+  <?php require('template.php'); ?>
