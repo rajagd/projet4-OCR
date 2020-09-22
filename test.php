fc3ca7e20253991d258f6a0868d5e4e01470aab1
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-23 00:10:24 +0200

    test file pour la BDD connection

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+require 'Database.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Jean ForteRoche</title>
+</head>
+
+<body>
+<div>
+    <h1>Mon blog</h1>
+    <p>En construction</p>
+    
+</div>
+</body>
+</html>
