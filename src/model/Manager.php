23defb46478763a91782e6a758398265b3dfc752
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-25 22:21:55 +0200

    création de constants DB
    création de constants DB HOST, USER et PASS ( mot de passe )

--- src/model/Manager.php
+++ src/model/Manager.php
@@ -1,17 +1,27 @@
 <?php
-//connection à la base de donnée
 class Manager
 {
-	protected function dbConnection() 
-	{
-		try
-		{
-			$bdd = new PDO('mysql:host=localhost;dbname=jeansblog;charset=utf8', 'root', '');
-			return $bdd;
-		}
-		catch (Exception $e)
-		{
-			die('Erreur : ' .$e->getMessage());
-		}
-	}
+    //Nos constantes
+
+    const DB_HOST = 'mysql:host=localhost;dbname=jeansblog;charset=utf8';
+    const DB_USER = 'root';
+    const DB_PASS = 'root';
+
+    //Méthode de connexion à notre base de données
+    public function getConnection()
+    {
+        //Tentative de connexion à la base de données
+
+        try {
+            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //On renvoie la connexion
+            return $connection;
+        } //On lève une erreur si la connexion échoué
+
+        catch (Exception $e) {
+            die('Erreur : ' .$e->getMessage());
+        }
+
+    }
 }
